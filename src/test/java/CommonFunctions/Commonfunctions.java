3d9cb16ebec8113b6606b53d37523759d9527354
karthik <karthikgreat24895@gmail.com>
2020-11-18 22:19:38 +0530

    Added Log4j

--- src/test/java/CommonFunctions/Commonfunctions.java
+++ src/test/java/CommonFunctions/Commonfunctions.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
@@ -20,6 +22,7 @@ import org.testng.annotations.BeforeSuite;
 public class Commonfunctions {
 	public static WebDriver driver=null;
 	public Properties properties;
+	static Logger logger=Logger.getLogger(Commonfunctions.class);
 	
 	public  Properties loadProperties() throws IOException
 	{
@@ -33,6 +36,7 @@ public class Commonfunctions {
 	public void launchbrowser() throws IOException 
 	{
 		loadProperties();
+		PropertyConfigurator.configure("log4j.properties");
 		String browser=properties.getProperty("browser");
 		if(browser.equalsIgnoreCase("chrome"))
 		{
@@ -40,6 +44,7 @@ public class Commonfunctions {
 			driver.manage().window().maximize();
 		}
 		driver.get(properties.getProperty("url"));
+		logger.info("Launching Browser");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 				
 	}
